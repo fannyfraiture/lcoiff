2a193c494e9e7a8b8a9cb826a37b6423cd2cab42
ThomasBackers <thomasbackers@gmail.com>
2022-03-09 15:30:41 +0100

    template started ;)

---
+++ wp-content/themes/LCoiff/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L'Coiff</title>
+  </head>
+
+  <body>
+    <header class="header">
+      <nav class="navbar">
+        <ul class="navbar__list">
+          <li class="navbar__list__element">
+            <a class="navbar__list__element__link" href="#">
+              devenir cliente
+            </a>
+          </li>
+
+          <li class="navbar__list__element">
+            <a class="navbar__list__element__link" href="#">
+              qui sommes-nous
+            </a>
+          </li>
+
+          <li class="navbar__list__element">
+            <a class="navbar__list__element__link" href="#">
+              <img
+                class="navbar__list__element__link__logo"
+                src=""
+                alt="logo LCoiff'"
+              >
+            </a>
+          </li>
+
+          <li class="navbar__list__element">
+            <a class="navbar__list__element__link" href="#">
+              nous soutenir
+            </a>
+          </li>
+
+          <li class="navbar__list__element">
+            <a class="navbar__list__element__link" href="#">
+              contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
--- wp-content/themes/LCoiff/index.php
+++ wp-content/themes/LCoiff/index.php
@@ -1,1 +1,2 @@
-coucou 
+<main class="main">
+</main>
