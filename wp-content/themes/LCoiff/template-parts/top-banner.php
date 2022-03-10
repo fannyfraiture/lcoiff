56f45d46854562bdbba8183082313d7daea2e00f
Thomas Backers <thomasbackers@gmail.com>
2022-03-10 12:18:37 +0100

    fixed the top-banner

--- wp-content/themes/LCoiff/template-parts/top-banner.php
+++ wp-content/themes/LCoiff/template-parts/top-banner.php
@@ -1,7 +1,13 @@
+<?php
+$image = $args['image'];
+$texte = $args['texte'];
+$is_button = $args['is_button'];
+?>
+
 <figure class="top-banner">
   <img
     class="top-banner__image"
-    src="<?php echo $image; ?>"
+    src="<?php echo esc_url($image); ?>"
     alt="femme en train d'être coiffée"
   >
 
