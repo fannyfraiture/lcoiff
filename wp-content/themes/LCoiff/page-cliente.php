dbdaa3af3b274bca900a0a327dc667b7f82a556e
Thomas Backers <thomasbackers@gmail.com>
2022-03-14 11:57:51 +0100

    re-structured page-cliente.php

--- wp-content/themes/LCoiff/page-cliente.php
+++ wp-content/themes/LCoiff/page-cliente.php
@@ -2,46 +2,48 @@
 /*
 Template Name: Cliente
 */
-?>
 
-<?php
-//Query data
 $cliente_titre = get_field('cliente_titre');
 $cliente_banniere = get_field('cliente_banniere');
-
-// echo '<pre>';
-// var_dump($cliente_banniere);
-// echo '</pre>';
-
-get_header(); 
 ?>
+
+<?php get_header(); ?>
 
 <main class="cliente">
 
   <?php if ($cliente_titre) : ?>
     <?php
-    get_template_part(
-      'template-parts/top',
-      'banner',
-      array(
-        'image' => $cliente_titre['image']['url'],
-        'texte' => $cliente_titre['titre'],
-        'is_button' => false
-      )
-    );
+      get_template_part(
+        'template-parts/top',
+        'banner',
+        array(
+          'image' => $cliente_titre['image']['url'],
+          'texte' => $cliente_titre['titre'],
+          'is_button' => false
+        )
+      );
     ?>
   <?php endif; ?>
 
   <ul class="cliente-blocks">
-    <?php foreach ($cliente_banniere as $bandeau) : ?>
+    <?php for ($i = 0; $i < count($cliente_banniere); $i++) : ?>
       <li class="cliente-blocks__clients-block">
-        <img class="cliente-blocks__clients-block__image" src="<?php echo esc_url($bandeau["bandeau"]["image"]['url']); ?>" alt="<?php echo esc_attr($bandeau["bandeau"]['image']['alt']); ?>" />
-        <h2 class="cliente-blocks__clients-block__title"> <?php echo $bandeau["bandeau"]["titre"] ?></h2>
-        <p class="cliente-blocks__clients-block__paragraph"> <?php echo $bandeau["bandeau"]["texte"] ?></p>
+        <img
+          class="cliente-blocks__clients-block__image"
+          src="<?php echo esc_url($cliente_banniere[$i]["bandeau"]["image"]['url']); ?>"
+          alt="<?php echo esc_attr($cliente_banniere[$i]["bandeau"]['image']['alt']); ?>"
+        >
+        <h2 class="cliente-blocks__clients-block__title">
+          <?php echo $cliente_banniere[$i]["bandeau"]["titre"] ?>
+        </h2>
+        
+        <p class="cliente-blocks__clients-block__paragraph">
+          <?php echo $cliente_banniere[$i]["bandeau"]["texte"] ?>
+        </p>
       </li>
-    <?php endforeach; ?>
+    <?php endfor; ?>
   </ul>
 
-    </main>
+</main>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
