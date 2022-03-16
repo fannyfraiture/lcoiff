81f4d6a662ec1938a25a2c975b2a973b921e78ab
Meulemans Philippe <meulemansph@hotm.com>
2022-03-16 16:09:40 +0100

    header source image path update

--- wp-content/themes/LCoiff/src/js/components/accordion.js
+++ wp-content/themes/LCoiff/src/js/components/accordion.js
@@ -1,29 +1,46 @@
 const titres = document.querySelectorAll(".info-blocks__info-block__title");
-const underlines = document.querySelectorAll(".info-blocks__info-block__title__underline")
-const paragraph = document.querySelectorAll(".info-blocks__info-block__paragraph");
+const underlines = document.querySelectorAll(
+  ".info-blocks__info-block__title__underline"
+);
+const paragraph = document.querySelectorAll(
+  ".info-blocks__info-block__paragraph"
+);
 const crosses = document.querySelectorAll(".info-blocks__info-block__plus");
-const cross = crosses[0];
 
 for (let i = 0; i < titres.length; i++) {
   //default value
-  paragraph[i].style.display = "none";
-  crosses[i].style.transform = "rotate(90deg)";
+  // paragraph[i].style.display = "none";
+  crosses[i].style.transform = "rotate(0deg)";
   underlines[i].style.width = "100%";
+  paragraph[i].style.transform = "translateY(-500px)";
+  paragraph[i].style.height = "0%";
   console.log(paragraph[i].style.display);
   console.log(crosses[i].style.transform);
 
   titres[i].addEventListener("click", () => {
-    paragraph[i].style.display =
-      paragraph[i].style.display === "block" ? "none" : "block";
+    // paragraph[i].style.display =
+    //   paragraph[i].style.display === "block" ? "none" : "block";
+    paragraph[i].style.height =
+      paragraph[i].style.height === "100%" ? "0%" : "100%";
+
+    paragraph[i].style.transform =
+      paragraph[i].style.transform === "translateY(0px)"
+        ? "translateY(-500px)"
+        : "translateY(0px)";
+
     crosses[i].style.transform =
-      crosses[i].style.transform === "rotate(90deg)"
-        ? "rotate(45deg)"
-        : "rotate(90deg)";
-    underlines[i].style.width =    
-      underlines[i].style.width === "50%"
-        ? "100%"
-        : "50%";
-});
+      crosses[i].style.transform === "rotate(45deg)"
+        ? "rotate(0deg)"
+        : "rotate(45deg)";
+    underlines[i].style.width =
+      underlines[i].style.width === "50%" ? "100%" : "50%";
+  });
+  //   titres[i].addEventListener("mouseenter", () => {
+
+  //     underlines[i].style.width = "75%";
+
+  //     setTimeout (() => {
+  //       underlines[i].style.width = "100%"
+  //     }, 2000);
+  // },false);
 }
-
-
